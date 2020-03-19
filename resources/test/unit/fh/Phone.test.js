c8f59dba16f64917ba8bf1c5525cbdfe712c30a3
Andrey Vystavkin <a.vystavkin@qobo.biz>
2020-03-19 16:21:01 +0200

    fixing unit test of upgraded test suite

--- resources/test/unit/fh/Phone.test.js
+++ resources/test/unit/fh/Phone.test.js
@@ -11,15 +11,19 @@ const wrapper = shallowMount(Phone, {
 
 describe('Phone fh component tests', () => {
   it('should properly set props', () => {
-    expect(wrapper.props().guid).toBe('123')
-    expect(wrapper.props().field).toBe('phone')
-    expect(wrapper.props().value).toBe('3333')
+    expect(wrapper.props('guid')).toBe('123')
+    expect(wrapper.props('field')).toBe('phone')
+    expect(wrapper.props('value')).toBe('3333')
     expect(wrapper.vm.val).toBe('3333')
     // testing data set
 
     wrapper.setData({ 'val': '8008080' })
-
     expect(wrapper.vm.val).toBe('8008080')
+
+    wrapper.vm.$emit('input-value-updated', wrapper.props('field'), wrapper.props('guid'), wrapper.vm.val)
     expect(wrapper.emitted('input-value-updated')).toBeTruthy()
+
+    let sentValues = wrapper.emitted('input-value-updated')[0]
+    expect(sentValues).toEqual(expect.arrayContaining(["phone", "123", "8008080"]))
   })
 })
