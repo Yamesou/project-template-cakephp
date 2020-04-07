3acff4a0d1604eb38338759fae7f653092613b06
Andrey Vystavkin <a.vystavkin@qobo.biz>
2020-04-07 20:45:14 +0300

    fixing lints for the components (task #15912)

--- resources/src/components/Dashboard/DashboardWidget.vue
+++ resources/src/components/Dashboard/DashboardWidget.vue
@@ -1,14 +1,32 @@
 <template>
-  <div class="box box-solid available-widget" v-bind:class="getElementBackground(item)">
-      <div class='box-header with-border'>
-          <h3 class="box-title"><i class="fa" v-bind:class="getElementIcon(item)"></i> {{item.title}}</h3>
-          <div class="box-tools">
-              <div class="btn btn-box-tool"><a href="#" @click="addGridItem(item)"><i class='fa fa-plus-circle'></i></a></div>
-          </div>
+  <div
+    class="box box-solid available-widget"
+    :class="getElementBackground(item)"
+  >
+    <div
+      class="box-header with-border"
+    >
+      <h3 class="box-title">
+        <i
+          :class="getElementIcon(item)"
+          class="fa"
+        />
+        {{ item.title }}
+      </h3>
+      <div class="box-tools">
+        <div class="btn btn-box-tool">
+          <a
+            href="#"
+            @click="addGridItem(item)"
+          >
+            <i class="fa fa-plus-circle" />
+          </a>
+        </div>
       </div>
-      <div class="box-body">
-          <p>{{item.data.name}}</p>
-      </div>
+    </div>
+    <div class="box-body">
+      <p>{{ item.data.name }}</p>
+    </div>
   </div>
 </template>
 <script>
