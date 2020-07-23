e33486a3b22a2e0f8c52cefc0e100207e2276e7f
Elia Mariutti <e.mariutti@qobo.biz>
2020-07-23 13:09:54 +0300

    Adjust `version` as integer (task #15054)

--- config/settings.php
+++ config/settings.php
@@ -141,7 +141,7 @@ return [
                     ],
                     'Version' => [
                         'alias' => 'Ldap.version',
-                        'type' => 'string',
+                        'type' => 'integer',
                         'help' => '',
                         'scope' => [
                             'app',
