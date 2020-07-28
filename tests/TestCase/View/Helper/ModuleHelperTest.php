203bf8d3eb49635b034c7ee308a028fb3bc69652
Timur Asaliev <t.asaliev@qobo.biz>
2020-07-28 10:53:45 +0300

    Add module helper (task #16669)

---
+++ tests/TestCase/View/Helper/ModuleHelperTest.php
@@ -0,0 +1,84 @@
+<?php
+namespace App\Test\TestCase\View\Helper;
+
+use App\View\Helper\ModuleHelper;
+use Cake\TestSuite\TestCase;
+use Cake\View\View;
+use Qobo\Utils\Module\ModuleRegistry;
+
+/**
+ * App\View\Helper\ModuleHelper Test Case
+ */
+class ModuleHelperTest extends TestCase
+{
+    /**
+     * Test subject
+     *
+     * @var \App\View\Helper\ModuleHelper
+     */
+    public $Module;
+
+    /**
+     * setUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $view = new View();
+        $this->Module = new ModuleHelper($view);
+
+        // Make sure the module is loaded with correct class
+        ModuleRegistry::getModule('Things');
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->Module);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test initial setup
+     *
+     * @return void
+     */
+    public function testInitialization(): void
+    {
+    }
+
+    /**
+     * Test field label
+     *
+     * @return void
+     */
+    public function testFieldLabel(): void
+    {
+        // Check field which has a field config
+        $label = $this->Module->fieldLabel('Things', 'name');
+        $this->assertEquals('label name', $label);
+    }
+
+    /**
+     * Test association label
+     *
+     * @return void
+     */
+    public function testAssociationLabel(): void
+    {
+        // Check association which has a label
+        $label = $this->Module->associationLabel('Things', 'PrimaryThingThings');
+        $this->assertEquals('Primary Thing Things', $label);
+
+        // Check association which is not a module
+        $label = $this->Module->associationLabel('Groups', 'Users');
+        $this->assertEquals('Users', $label);
+    }
+}
